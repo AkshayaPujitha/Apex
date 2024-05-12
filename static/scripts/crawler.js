3155dc2679f9bd051b456b1248b655529ae98759
pujitha <akshayapujithakolli@gmail.com>
2024-05-12 11:05:41 +0530

    Results been appered in frontend

--- static/scripts/crawler.js
+++ static/scripts/crawler.js
@@ -1,32 +1,27 @@
-document.getElementById('crawlForm').onsubmit = function(event) {
-    event.preventDefault();  // Prevent the default form submission behavior
-    
-    // Gather data from the form
-    const seedUrl = document.getElementById('seed_url').value;
-    const keywordsInput = document.getElementById('keywords').value;
-    const keywords = keywordsInput.split(',').map(item => item.trim());  // Clean the keywords
+document.getElementById("crawlerForm").addEventListener("submit", async function (event) {
+    event.preventDefault();
 
-    // Use fetch API to send a POST request to your Flask server
-    fetch('/crawl', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ seed_url: seedUrl, keywords: keywords })
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
+    const formData = {
+        seed_url: document.getElementById("seed_url").value,
+        keywords: document.getElementById("keywords").value.split(",").map(kw => kw.trim())
+    };
+
+    try {
+        const response = await fetch("/crawl", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        });
+        
+        console.log(response)
+        if (response.ok) {
+            window.location.href = "/result";
+        } else {
+            throw new Error('Server error or invalid response.');
         }
-        return response.json();  // Parse JSON data from the response
-    })
-    .then(data => {
-        // Redirect to the results page if the request was successful
-        window.location.href = "/result";  // Ensure the route is correct as per your Flask setup
-    })
-    .catch(error => {
-        // Handle any errors that occurred during the fetch
-        console.error('Error:', error);
-        alert('There was a problem with your fetch operation: ' + error.message);
-    });
-};
+    }
+    catch (error) {
+    }
+});
