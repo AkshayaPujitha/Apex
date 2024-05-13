141ee33a7ebbf082744c64b28bb45d31a843715f
pujitha <akshayapujithakolli@gmail.com>
2024-05-13 22:34:29 +0530

    test case for webcrawl added

--- test_cases/webcrawl_test.py
+++ test_cases/webcrawl_test.py
@@ -9,16 +9,13 @@ class TestCrawler(unittest.TestCase):
         self.crawler = Crawler(self.url_to_crawl, self.keyword)
 
     def test_get_html_content(self):
-        # Mocking the requests.Session.get method to return a sample HTML content
         self.crawler.session.get = MagicMock(return_value=MagicMock(text='<html><body><p>This is a test</p></body></html>'))
 
-        # Asserting that the method returns the expected HTML content
         html_content = self.crawler.get_html_content('https://www.w3schools.com/')
         self.assertEqual(html_content, '<html><body><p>This is a test</p></body></html>')
 
     
 
-    # Add more test cases as needed
 
 if __name__ == '__main__':
     unittest.main()
